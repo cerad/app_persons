d117adae459e75a5f072d7de41e7d81f3d5e2c74
ahundiak <ahundiak@spike.IGSLAN.com>
2015-03-25 15:22:29 -0500

    Routes and services moved to their own files. Added database_connection service. Referee repository working. Kernel events and CORS listener.

--- web/app-config.dist.js
+++ web/app-config.dist.js
@@ -2,6 +2,6 @@
 
 var configModule = angular.module('appConfigModule', []);
 
-configModule.constant('ceradApiPrefix','/app_dev.php');
+configModule.constant('ceradApiPrefix','http://localhost:8001/app.php');
 
 })(angular);
